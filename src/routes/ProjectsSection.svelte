dce2c4d9dbf81a4fc5ca9a64110ec9c1aadc17d9
work <s.a.fivaz@gmail.com>
2024-04-06 00:11:23 +0200

    project scrum flow done

--- src/routes/ProjectsSection.svelte
+++ src/routes/ProjectsSection.svelte
@@ -4,53 +4,194 @@
 	import scrumFlowImage2 from '$lib/assets/scrum-flow-2.png';
 </script>
 
-<section class="p-5 h-96 bg-white w-full flex justify-center">
-	<div class="md:w-[880px] flex flex-col items-center">
-		<h1 class="text-3xl font-bold text-center my-5">Projects</h1>
+<section class="bg-white w-full">
+	<h1 class="text-3xl font-bold text-center my-5">Projects</h1>
 
-		<div class="flex gap-5 flex-col md:flex-row items-start">
-			<div>
-				<img
-					alt="scrum-flow page 1"
-					class="rounded-lg hidden md:block w-96"
-					src={scrumFlowImage1}
-				/>
-				<img alt="scrum-flow page 2" class="rounded-lg w-96" src={scrumFlowImage2} />
+	<div class="md:w-[880px] m-auto border rounded-lg">
+		<div class="relative isolate overflow-hidden px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+			<div class="absolute inset-0 -z-10 overflow-hidden">
+				<svg
+					aria-hidden="true"
+					class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+				>
+					<defs>
+						<pattern
+							height="200"
+							id="e813992c-7d03-4cc4-a2bd-151760b470a0"
+							patternUnits="userSpaceOnUse"
+							width="200"
+							x="50%"
+							y="-1"
+						>
+							<path d="M100 200V.5M.5 .5H200" fill="none" />
+						</pattern>
+					</defs>
+					<svg class="overflow-visible fill-gray-50" x="50%" y="-1">
+						<path
+							d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+							stroke-width="0"
+						/>
+					</svg>
+					<rect
+						fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"
+						height="100%"
+						stroke-width="0"
+						width="100%"
+					/>
+				</svg>
 			</div>
-			<div>
-				<h2 class="text-2xl font-semibold mb-2">Scrum Flow</h2>
-				<p>Web application designed to enhance Scrum project management within Jira</p>
-				<ul class="list-disc">
-					<li>Seamlessly integrated with Jira Software through Atlassian OAuth.</li>
-					<li>
-						Features graphical representations illustrating the evolution of estimation maturity for
-						the Scrum Team, aiding in visualizing team progress over time.
-					</li>
-					<li>
-						Provides insightful information on the accuracy of task estimates, proving particularly
-						valuable during Sprint Retrospectives for assessing the efficacy of completed sprints.
-						This facilitates informed decision-making and enhances planning within the Scrum
-						framework.
-					</li>
-					<li>
-						Includes a predictive module employing Machine Learning and Data Analysis to forecast
-						the time required for upcoming tasks in a Sprint. This innovative feature contributes to
-						proactive planning and resource management.
-					</li>
-				</ul>
-				<div class="flex justify-center gap-5 p-5">
-					<a href="https://scrum-flow-thesis.sfivaz.com" target="_blank">
-						<Button>Documentation</Button>
-					</a>
-					<a href="https://scrum-flow.sfivaz.com/demo" target="_blank">
-						<Button>Demo</Button>
-					</a>
+			<div
+				class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10"
+			>
+				<div
+					class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8"
+				>
+					<div class="lg:pr-4">
+						<div class="lg:max-w-lg">
+							<p class="text-base font-semibold leading-7 text-indigo-600">Tool for Jira</p>
+							<h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+								Scrum Flow
+							</h1>
+							<p class="mt-6 text-xl leading-8 text-gray-700">
+								Web application designed to enhance Scrum project management within Jira.
+							</p>
+							<div>
+								<img
+									alt="tech stack"
+									class="mt-5 m-auto h-8"
+									src="https://skillicons.dev/icons?i=react,ts,next,tailwind,python,postgres"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div
+					class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden"
+				>
+					<img
+						alt=""
+						class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+						src={scrumFlowImage2}
+					/>
+				</div>
+				<div
+					class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8"
+				>
+					<div class="lg:pr-4">
+						<div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
+							<ul class="mt-8 space-y-8 text-gray-600" role="list">
+								<li class="flex gap-x-3">
+									<svg
+										aria-hidden="true"
+										class="mt-1 h-5 w-5 flex-none text-indigo-600"
+										fill="currentColor"
+										viewBox="0 0 20 20"
+									>
+										<path
+											clip-rule="evenodd"
+											d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
+											fill-rule="evenodd"
+										/>
+									</svg>
+									<span>
+										<strong class="font-semibold text-gray-900">No Setup.</strong>
+										Seamlessly integrated with Jira Software through Atlassian OAuth, automatically handling
+										data analysis without requiring additional setup or manual data migration.
+									</span>
+								</li>
+								<li class="flex gap-x-3">
+									<svg
+										aria-hidden="true"
+										class="mt-1 h-5 w-5 flex-none text-indigo-600"
+										fill="currentColor"
+										viewBox="0 0 20 20"
+									>
+										<path
+											clip-rule="evenodd"
+											d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+											fill-rule="evenodd"
+										/>
+									</svg>
+									<span>
+										<strong class="font-semibold text-gray-900">Visual analysis.</strong>
+										Features graphical representations illustrating the evolution of estimation maturity
+										for the Scrum Team, aiding in visualizing team progress over time.
+									</span>
+								</li>
+								<li class="flex gap-x-3">
+									<svg
+										aria-hidden="true"
+										class="mt-1 h-5 w-5 flex-none text-indigo-600"
+										fill="currentColor"
+										viewBox="0 0 20 20"
+									>
+										<path
+											d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z"
+										/>
+										<path
+											clip-rule="evenodd"
+											d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z"
+											fill-rule="evenodd"
+										/>
+									</svg>
+									<span>
+										<strong class="font-semibold text-gray-900">Accuracy estimation.</strong>
+										Provides insightful information on the accuracy of task estimates, proving particularly
+										valuable during Sprint Retrospectives for assessing the efficacy of completed sprints.
+										This facilitates informed decision-making and enhances planning within the Scrum
+										framework.
+									</span>
+								</li>
+								<li class="flex gap-x-3">
+									<svg
+										aria-hidden="true"
+										class="mt-1 h-5 w-5 flex-none text-indigo-600"
+										fill="currentColor"
+										viewBox="0 0 20 20"
+									>
+										<path
+											d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z"
+										/>
+										<path
+											clip-rule="evenodd"
+											d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z"
+											fill-rule="evenodd"
+										/>
+									</svg>
+									<span>
+										<strong class="font-semibold text-gray-900">Sprint Prediction.</strong>
+										Includes a predictive module employing Machine Learning and Data Analysis to forecast
+										the time required for upcoming tasks in a Sprint. This innovative feature contributes
+										to proactive planning and resource management.
+									</span>
+								</li>
+							</ul>
+							<h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+								Got interested ?
+							</h2>
+							<p class="mt-6 mb-3">
+								Explore the detailed documentation (in French) and try out the live demo
+							</p>
+							<div class="flex gap-3">
+								<a
+									class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold border border-indigo-600 text-indigo-600 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+									href="https://scrum-flow-thesis.sfivaz.com"
+									target="_blank"
+								>
+									Documentation
+								</a>
+								<a
+									class="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+									href="https://scrum-flow.sfivaz.com/demo"
+									target="_blank"
+								>
+									Demo
+								</a>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
-
-		<div></div>
-
-		<div></div>
 	</div>
 </section>

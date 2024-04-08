588203e6c79d7aea4cb6541a57bdbb7ec4c801b2
work <s.a.fivaz@gmail.com>
2024-04-09 00:28:14 +0200

    add icons

--- src/routes/ProjectsSection.svelte
+++ src/routes/ProjectsSection.svelte
@@ -1,5 +1,8 @@
 <script>
 	import scrumFlowImage2 from '$lib/assets/scrum-flow-2.png';
+	import {Wrench, WrenchScrewdriver} from "@steeze-ui/heroicons";
+	import {AreaChart, Target, TrendingUp} from '@steeze-ui/lucide-icons'
+	import { Icon } from '@steeze-ui/svelte-icon';
 </script>
 
 <section class="w-full bg-white">
@@ -79,18 +82,7 @@
 						<div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
 							<ul class="mt-8 space-y-8 text-gray-600" role="list">
 								<li class="flex gap-x-3">
-									<svg
-										aria-hidden="true"
-										class="mt-1 h-5 w-5 flex-none text-indigo-600"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-									>
-										<path
-											clip-rule="evenodd"
-											d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-											fill-rule="evenodd"
-										/>
-									</svg>
+									<Icon class="mt-1 h-5 w-5 flex-none text-indigo-600" src={WrenchScrewdriver}/>
 									<span>
 										<strong class="font-semibold text-gray-900">No Setup.</strong>
 										Seamlessly integrated with Jira Software through Atlassian OAuth, automatically handling
@@ -98,18 +90,7 @@
 									</span>
 								</li>
 								<li class="flex gap-x-3">
-									<svg
-										aria-hidden="true"
-										class="mt-1 h-5 w-5 flex-none text-indigo-600"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-									>
-										<path
-											clip-rule="evenodd"
-											d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-											fill-rule="evenodd"
-										/>
-									</svg>
+									<Icon class="mt-1 h-5 w-5 flex-none text-indigo-600" src={AreaChart}/>
 									<span>
 										<strong class="font-semibold text-gray-900">Visual analysis.</strong>
 										Features graphical representations illustrating the evolution of estimation maturity
@@ -117,21 +98,7 @@
 									</span>
 								</li>
 								<li class="flex gap-x-3">
-									<svg
-										aria-hidden="true"
-										class="mt-1 h-5 w-5 flex-none text-indigo-600"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-									>
-										<path
-											d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z"
-										/>
-										<path
-											clip-rule="evenodd"
-											d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z"
-											fill-rule="evenodd"
-										/>
-									</svg>
+									<Icon class="mt-1 h-5 w-5 flex-none text-indigo-600" src={Target}/>
 									<span>
 										<strong class="font-semibold text-gray-900">Accuracy estimation.</strong>
 										Provides insightful information on the accuracy of task estimates, proving particularly
@@ -141,21 +108,7 @@
 									</span>
 								</li>
 								<li class="flex gap-x-3">
-									<svg
-										aria-hidden="true"
-										class="mt-1 h-5 w-5 flex-none text-indigo-600"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-									>
-										<path
-											d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z"
-										/>
-										<path
-											clip-rule="evenodd"
-											d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z"
-											fill-rule="evenodd"
-										/>
-									</svg>
+									<Icon class="mt-1 h-5 w-5 flex-none text-indigo-600" src={TrendingUp}/>
 									<span>
 										<strong class="font-semibold text-gray-900">Sprint Prediction.</strong>
 										Includes a predictive module employing Machine Learning and Data Analysis to forecast

a51ccd111e45e7104d6bfadcc9df75268c9de079
work <s.a.fivaz@gmail.com>
2024-04-09 10:05:48 +0200

    life wip

--- src/routes/Life.svelte
+++ src/routes/Life.svelte
@@ -21,11 +21,6 @@
 			name: 'Progressive Web App (PWA)',
 		},
 		{
-			description: 'Seamlessly integrates a TODO list for effective task management within the planner.',
-			icon: TouchIcon,
-			name: 'TODO List Integration',
-		},
-		{
 			description: 'Allows users to set and track goals, with the flexibility to create events or tasks attached to those goals.',
 			icon: TouchIcon,
 			name: 'Goal Management',
@@ -44,61 +39,30 @@
 
 </script>
 
-
-crafted with Svelte, Typescript, Tailwind, Firebase, and Storybook.
-Technology Proficiency:
-<div class='bg-white py-24 sm:py-32'>
+<div class='bg-white pt-14 pb-24 sm:pt-14 sm:pb-32'>
 	<div class='mx-auto max-w-7xl px-6 lg:px-8'>
 		<div class='mx-auto max-w-2xl lg:mx-0 lg:max-w-none'>
 			<p class='text-base font-semibold leading-7 text-indigo-600'>Day planer</p>
 			<h1 class='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>Life</h1>
 			<img
-				alt="tech stack"
-				class="mt-5 h-10"
-				src="https://skillicons.dev/icons?i=svelte,ts,tailwind,firebase"
+				alt='tech stack'
+				class='mt-5 h-10'
+				src='https://skillicons.dev/icons?i=svelte,ts,tailwind,firebase'
 			/>
-			<div class='mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2'>
-				<div>
-					<p>
-						A sophisticated Day Planner offline-first web application
-					</p>
+			<p class='mt-6 text-xl leading-8 text-gray-700'>
+				A sophisticated Day Planner fully responsive offline-first web application
+			</p>
 
-					<p class='mt-8'>Featuring an aesthetically pleasing calendar design, providing
-						an intuitive and visually appealing user experience.
-						Built with Svelte, showcasing my ability to excel in a framework with a growing ecosystem. This project highlights my skill in maximizing language capabilities without relying heavily on third-party libraries.
-					</p>
-				</div>
-
-				<div>
-					<p>
-						This project exemplifies my proficiency in frontend development, user interface design, and seamless
-						integration with cloud services. Leveraging modern frameworks and libraries ensures a polished and
-						responsive user experience.
-					</p>
-
-					<h2 class='mt-16 text-2xl font-bold tracking-tight text-gray-900'>
-						Got interested ?
-					</h2>
-					<p class='mb-3 mt-6'>
-						Explore the live demo and delve into the codebase on GitHub.
-					</p>
-					<div class='flex gap-3'>
-						<a
-							class='rounded-md border border-indigo-600 bg-white px-2.5 py-1.5 text-sm font-semibold text-indigo-600 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
-							href='https://github.com/fivaz/life'
-							target='_blank'
-						>
-							Source code
-						</a>
-						<a
-							class='rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-							href='https://life.sfivaz.com/demo'
-							target='_blank'
-						>
-							Demo
-						</a>
-					</div>
-				</div>
+			<div class='mt-6 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2'>
+				<p>Featuring an aesthetically pleasing calendar design, providing
+					an intuitive and visually appealing user experience.
+					Built with <strong>Svelte</strong>, showcasing my ability to excel in a framework with a growing ecosystem.
+				</p>
+				<p>
+					This project highlights my skill in maximizing language capabilities without relying heavily on third-party libraries.
+					Moreover, Life exemplifies my proficiency in frontend development, user interface design, and seamless
+					integration with cloud services.
+				</p>
 			</div>
 		</div>
 	</div>
@@ -138,6 +102,33 @@ Technology Proficiency:
 					<p class='mt-2 text-gray-700'>{feature.description}</p>
 				</li>
 			{/each}
+
+			<li
+				class='rounded-2xl border border-gray-200 p-8'
+			>
+				<h2 class='text-2xl font-bold tracking-tight text-gray-900'>
+					Got interested ?
+				</h2>
+				<p class='mb-3 mt-6'>
+					Explore the live demo and delve into the codebase on GitHub.
+				</p>
+				<div class='flex gap-3'>
+					<a
+						class='rounded-md border border-indigo-600 bg-white px-2.5 py-1.5 text-sm font-semibold text-indigo-600 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+						href='https://github.com/fivaz/life'
+						target='_blank'
+					>
+						Source code
+					</a>
+					<a
+						class='rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+						href='https://life.sfivaz.com/demo'
+						target='_blank'
+					>
+						Demo
+					</a>
+				</div>
+			</li>
 		</ul>
 	</div>
 

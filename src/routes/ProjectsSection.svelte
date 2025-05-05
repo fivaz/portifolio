62b2d41933d97e0bd43ae0bbe9265e6b2e760450
fivaz <s.a.fivaz@gmail.com>
2025-05-05 14:05:25 +0200

    wip

--- src/routes/ProjectsSection.svelte
+++ src/routes/ProjectsSection.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Life from './Life.svelte';
 	import ScrumFlow from './ScrumFlow.svelte';
+	import Routine from './Routine.svelte';
 </script>
 
 <!--scroll-mt-16 makes the anchor div appear slight under the viewport so it's not covered by the fixed navbar -->
@@ -8,6 +9,8 @@
 	<h1 class="mt-10 text-center text-3xl font-bold">Projects</h1>
 
 	<ScrumFlow />
+
+	<Routine />
 
 	<Life />
 </section>
---
+++ src/routes/Routine.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import routineImage1 from '$lib/assets/routine-1.png';
+	import routineImage2 from '$lib/assets/routine-2.png';
+	import PhoneFrame from '$lib/PhoneFrame.svelte';
+	import SmallPhoneFrame from '$lib/SmallPhoneFrame.svelte';
+	import PwaIcon from '$lib/PwaIcon.svelte';
+	import LifeTechStackIcons from '$lib/LifeTechStackIcons.svelte';
+</script>
+
+<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+	<div class="flex justify-between">
+		<div>
+			<p class="text-base font-semibold leading-7 text-indigo-600">Turn Habits into Flow</p>
+			<h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+				RoutineMaster
+			</h1>
+			<div class="mt-5 flex h-10 gap-2">
+				<LifeTechStackIcons />
+				<PwaIcon class="h-10 w-10 rounded-lg bg-gray-200 p-1" />
+			</div>
+			<p>
+				A productivity mobile web app that helps users build focus-driven routines with countdowns,
+				real-time progress tracking, and AI-generated visuals
+			</p>
+			<h2>Key Technologies:</h2>
+			<p>React (TypeScript), Java (Spring Boot), Firebase, Gemini API, Recraft AI</p>
+			<p>Sentry, Docker, GitHub Actions, Swagger UI</p>
+			<h2>Core Features (use icons for each):</h2>
+			<p>⏱️ Countdown per task</p>
+			<p>🔁 Real-time updates (Firestore listeners)</p>
+			<p>🎨 AI-generated task visuals</p>
+			<p>📈 Performance graphs</p>
+			<p>🛠️ CI/CD with GitHub Actions & Docker</p>
+			<p>🧪 Error monitoring with Sentry</p>
+		</div>
+		<SmallPhoneFrame image={routineImage2} />
+	</div>
+</div>
